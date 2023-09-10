8ab238ff62cb6d47938b26e98136fb81e725c94d
Mark Grimke <MarsAltar01@gmail.com>
2023-09-10 13:08:05 -0400

    updated test in package to stop erroring on circleCI

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const spongegar = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
 
 // Handle responses
-const getSponge = (response) =>{
-    response.writeHead(200, { 'Content-Type': 'image/png' });
-    response.write(spongegar);
-    response.end();
+const getSponge = (response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(spongegar);
+  response.end();
 };
 
 module.exports = {
-    getSponge
-}
+  getSponge,
+};
